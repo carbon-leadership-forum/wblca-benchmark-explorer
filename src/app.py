f5356c91f091b736cfde40651c769d1a91798366
Mel Chafart <mchaf@carbonleadershipforum.org>
2025-07-02 11:58:42 -0700

    feat: add google analytics to app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-import pandas as pd
 from dash import Dash, html, page_container, dcc, State
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
@@ -13,39 +11,43 @@ app = Dash(
         # The app state is serialised in the URL hash without refreshing the page
         # This URL can be copied and then parsed on page load
         "state": State("main-url", "hash"),
-   },
+    },
 )
 server = app.server
 
-load_figure_template('bootstrap')
+load_figure_template("bootstrap")
 
 header = create_header()
 
 app.layout = dbc.Container(
     [
-        dcc.Store(
-            id='byob_data',
-            storage_type='memory',
-        ),
         dcc.Location(id="main-url"),
         dbc.Row(
             html.Header(
                 dbc.Row(
                     dbc.Col(
                         header,
-                        className='mb-2',
-                        xs=12, sm=12, md=12, lg=12, xl=10, xxl=10
+                        className="mb-2",
+                        xs=12,
+                        sm=12,
+                        md=12,
+                        lg=12,
+                        xl=10,
+                        xxl=10,
                     ),
-                    justify='center'
+                    justify="center",
                 ),
             )
         ),
-        dbc.Row(
-            page_container
-        )
+        dbc.Row(page_container),
     ],
     fluid=True,
-    className='dbc'
+    className="dbc",
+)
+app.scripts.append_script(
+    {
+        "external_url": "https://wblca-benchmark-explorer.carbonleadershipforum.org//assets/gtag.js"
+    }
 )
 
 
