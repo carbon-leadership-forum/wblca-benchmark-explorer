1862766cc54eda589626079607c9d613cfe12b06
Mel Chafart <mchaf@carbonleadershipforum.org>
2025-07-02 12:26:36 -0700

    fix: update gtag.js for correct analytics

--- src/assets/gtag.js
+++ src/assets/gtag.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'G-PYW19VH4S5', 'auto');
+ga('create', 'G-KMWHP8B0DJ', 'auto');
 ga('send', 'pageview');
